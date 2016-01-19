f46c46b5173b44c9c75f29e550021da130493c39
tsato1 <tsato1@ucsc.edu>
2016-01-18 21:32:17 -0800

    buttons created, color and size set, toast shown on click

--- app/src/main/java/com/udacity/tsato/myappportfolio/MainActivity.java
+++ app/src/main/java/com/udacity/tsato/myappportfolio/MainActivity.java
@@ -8,8 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button spotifyStreamerButton;
+    private Button scoresAppButton;
+    private Button libraryAppButton;
+    private Button buildItBiggerButton;
+    private Button xyzReaderButton;
+    private Button capstoneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +26,53 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        spotifyStreamerButton = (Button) findViewById(R.id.btn_spotify_streamer);
+        spotifyStreamerButton.setOnClickListener(new ButtonClickListener());
+        scoresAppButton = (Button) findViewById(R.id.btn_scores_app);
+        scoresAppButton.setOnClickListener(new ButtonClickListener());
+        libraryAppButton = (Button) findViewById(R.id.btn_library_app);
+        libraryAppButton.setOnClickListener(new ButtonClickListener());
+        buildItBiggerButton = (Button) findViewById(R.id.btn_build_it_bigger);
+        buildItBiggerButton.setOnClickListener(new ButtonClickListener());
+        xyzReaderButton = (Button) findViewById(R.id.btn_xyz_reader);
+        xyzReaderButton.setOnClickListener(new ButtonClickListener());
+        capstoneButton = (Button) findViewById(R.id.btn_capstone);
+        capstoneButton.setOnClickListener(new ButtonClickListener());
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    public class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.btn_spotify_streamer:
+                    Toast.makeText(MainActivity.this, "Spotify streamer will be launched", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btn_scores_app:
+                    Toast.makeText(MainActivity.this, "Score App will be launched", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btn_library_app:
+                    Toast.makeText(MainActivity.this, "Library App will be launched", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btn_build_it_bigger:
+                    Toast.makeText(MainActivity.this, "Build It Bigger will be launched", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btn_xyz_reader:
+                    Toast.makeText(MainActivity.this, "XYZ Reader will be launched", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btn_capstone:
+                    Toast.makeText(MainActivity.this, "Capstone App will be launched", Toast.LENGTH_SHORT).show();
+                    break;
             }
-        });
+        }
     }
 
     @Override
